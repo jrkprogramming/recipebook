c8ef02f09d87d947ba90029c83c6d6ee3d05156b
jrkprogramming <jrkaibigan@gmail.com>
2023-06-20 16:38:31 -0500

    added trpc routes to get all recipes and create route

---
+++ src/server/api/routers/recipe.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import {
+	createTRPCRouter,
+	// publicProcedure,
+	protectedProcedure,
+} from "~/server/api/trpc";
+
+export const recipeRouter = createTRPCRouter({
+	getAll: protectedProcedure.query(({ ctx }) => {
+		return ctx.prisma.recipe.findMany({
+			where: {
+				userId: ctx.session.user.id
+			}
+		});
+	}),
+
+	create: protectedProcedure
+		.input(z.object({ mealName: z.string(), notes: z.string(), protein: z.number(), fat: z.number(), carbs: z.number(), calories: z.number() }))
+		.mutation(({ ctx, input }) => {
+			return ctx.prisma.recipe.create({
+				data: {
+					mealName: input.mealName,
+					notes: input.notes,
+					protein: input.protein,
+					fat: input.fat,
+					carbs: input.carbs,
+					calories: input.calories,
+					userId: ctx.session.user.id,
+
+				}
+			})
+		})
+})
